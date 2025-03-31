8cbc1b545daefa34b47854b851db78f42d3767f1
Vaibhav <vaibhavchaubey786@gmail.com>
2025-03-31 15:54:29 +0530

    refactor: rename JournalEntryController and add UserController for user management

---
+++ src/main/java/com/vaibhav/journalApp/entity/User.java
@@ -0,0 +1,28 @@
+package com.vaibhav.journalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+
+}
