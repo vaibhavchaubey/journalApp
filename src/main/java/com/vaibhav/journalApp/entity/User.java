96145867c80742a20e1b78a64958cb3c2d4cd943
Vaibhav <vaibhavchaubey786@gmail.com>
2025-04-10 23:17:01 +0530

    refactor: enhance User and UserService with builder pattern and error handling for user creation

--- src/main/java/com/vaibhav/journalApp/entity/User.java
+++ src/main/java/com/vaibhav/journalApp/entity/User.java
@@ -1,5 +1,6 @@
 package com.vaibhav.journalApp.entity;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
 @Document(collection = "users")
 public class User {
     @Id
--- src/main/java/com/vaibhav/journalApp/service/UserService.java
+++ src/main/java/com/vaibhav/journalApp/service/UserService.java
@@ -29,10 +29,15 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void saveNewUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+    public boolean saveNewUser(User user){
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public void saveAdmin(User user){
