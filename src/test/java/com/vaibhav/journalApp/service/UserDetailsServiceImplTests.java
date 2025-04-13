2ce6b8445b0d25bc8918130e534bc2aa35de6513
Vaibhav <vaibhavchaubey786@gmail.com>
2025-04-13 21:03:38 +0530

    test: add parameterized tests for user creation and user retrieval

---
+++ src/test/java/com/vaibhav/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,40 @@
+package com.vaibhav.journalApp.service;
+
+import com.vaibhav.journalApp.entity.User;
+import com.vaibhav.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.ArrayList;
+
+import static  org.mockito.Mockito.*;
+
+public class UserDetailsServiceImplTests {
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("vaibhav777").password("password123").roles(new ArrayList<>()).build());
+
+        UserDetails user =  userDetailsService.loadUserByUsername("vaibhav777");
+
+        Assertions.assertNotNull(user);
+
+    }
+
+}
